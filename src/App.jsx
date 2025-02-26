edcbdb4b5937306f006632b9ff19ef31540bed76
KshitijOza <kshitijoza04@gmail.com>
2025-02-26 22:21:52 +0530

    Refactor import statements in App.jsx to use consistent casing for component paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 // App.jsx - Main Component
-import React, { useState } from 'react';
-import HeroSection from './components/HeroSection';
-import BenefitsSection from './components/BenifitsSection';
-import HowItWorksSection from './components/HowItWorksSection';
-import FAQSection from './components/FAQSection';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import HeroSection from "./Components/HeroSection";
+import BenefitsSection from "./Components/BenifitsSection";
+import HowItWorksSection from "./Components/HowItWorksSection";
+import FAQSection from "./Components/FAQSection";
+import Footer from "./Components/Footer";
 
 const App = () => {
- 
   return (
     <div className="inter-font min-h-screen font-inter bg-gray-50">
       <HeroSection />
